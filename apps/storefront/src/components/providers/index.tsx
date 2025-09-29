52fde0fb42e9be20a8b8bc2f31b8be046a4b93d8
malawllaqi <malawllaqi@gmail.com>
2025-09-29 03:55:52 +0300

    bug: fixed query provider wrapper

--- apps/storefront/src/components/providers/index.tsx
+++ apps/storefront/src/components/providers/index.tsx
@@ -1,12 +1,8 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { ReactNode } from "react";
-
-const queryClient = new QueryClient();
+import { QueryProvider } from "./query-provider";
 
 export function Providers({ children }: { children: ReactNode }) {
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
+  return <QueryProvider>{children}</QueryProvider>;
 }
